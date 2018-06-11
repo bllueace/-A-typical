be91f69c80ba78f5d0ec970ddac98f3081e91cbb
Elvis <1600825@abertay.ac.uk>
2018-06-11 16:20:07 +0100

    gravity 1

--- HTC-Vive/Introduction To HTC Vive - Starter/Assets/Climbing.cs
+++ HTC-Vive/Introduction To HTC Vive - Starter/Assets/Climbing.cs
@@ -4,28 +4,40 @@ using UnityEngine;
 
 public class Climbing : MonoBehaviour {
 
-    public GameObject Body;
+    public Rigidbody Body;
     public SteamVR_TrackedObject controller;
 
     [HideInInspector] //no need to see it in inspector
     public Vector3 prevPos;
 
-    //[HideInInspector]
+    [HideInInspector]
     public bool canGrip;
-    public bool test;
+    //public bool test;
 
 	void Start ()
     {
         prevPos = controller.transform.localPosition;
 	}
 	
-	void Update ()
+	void FixedUpdate ()
     {
-        var device = SteamVR_Controller.Input((int)controller.index);
-        if (canGrip && device.GetPress(SteamVR_Controller.ButtonMask.Grip))
+        var device = SteamVR_Controller.Input((int)controller.index); //get controller
+        if (canGrip && device.GetPress(SteamVR_Controller.ButtonMask.Grip)) // #TODO GetPRessDown?
         {
+            Body.useGravity = false;
+            Body.isKinematic = true;
             Body.transform.position += (prevPos - controller.transform.localPosition);
-            //test = true;
+
+        }else if(canGrip && device.GetPressUp(SteamVR_Controller.ButtonMask.Grip)) //some physics 
+        {
+            Body.useGravity = true;
+            Body.isKinematic = false;
+            Body.velocity = (prevPos - controller.transform.localPosition) / Time.deltaTime;
+        }
+        else
+        {
+            Body.useGravity = true;
+            Body.isKinematic = false;
         }
         //if (device.GetPress(SteamVR_Controller.ButtonMask.Grip))
         //{
