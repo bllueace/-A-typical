13bebe7d3a49556aac7b0a9fb0b3bba8b2e6a897
Elvis <1600825@abertay.ac.uk>
2018-06-11 15:34:21 +0100

    Basic Climbing + Level 2 + Other Object
    Basic Climbing (no falling yet) and added a bunch of object to the scene for testing.

---
+++ HTC-Vive/Introduction To HTC Vive - Starter/Assets/Climbing.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Climbing : MonoBehaviour {
+
+    public GameObject Body;
+    public SteamVR_TrackedObject controller;
+
+    [HideInInspector] //no need to see it in inspector
+    public Vector3 prevPos;
+
+    //[HideInInspector]
+    public bool canGrip;
+    public bool test;
+
+	void Start ()
+    {
+        prevPos = controller.transform.localPosition;
+	}
+	
+	void Update ()
+    {
+        var device = SteamVR_Controller.Input((int)controller.index);
+        if (canGrip && device.GetPress(SteamVR_Controller.ButtonMask.Grip))
+        {
+            Body.transform.position += (prevPos - controller.transform.localPosition);
+            //test = true;
+        }
+        //if (device.GetPress(SteamVR_Controller.ButtonMask.Grip))
+        //{
+        //    test = true;
+        //}
+        prevPos = controller.transform.localPosition;
+    }
+
+    void OnTriggerEnter()
+    {
+        canGrip = true;
+    }
+    void OnTriggerExit()
+    {
+        canGrip = false;
+    }
+}
