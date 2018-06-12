077c4509b4047ec33becd9f52a898c961abdbfb2
Elvis <1600825@abertay.ac.uk>
2018-06-12 10:36:40 +0100

    climbing 1.1 + gravity 1.1

--- HTC-Vive/Introduction To HTC Vive - Starter/Assets/Climbing.cs
+++ HTC-Vive/Introduction To HTC Vive - Starter/Assets/Climbing.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Climbing : MonoBehaviour {
 
-    public Rigidbody Body;
     public SteamVR_TrackedObject controller;
 
     [HideInInspector] //no need to see it in inspector
@@ -17,38 +16,17 @@ public class Climbing : MonoBehaviour {
 	void Start ()
     {
         prevPos = controller.transform.localPosition;
-	}
-	
-	void FixedUpdate ()
-    {
-        var device = SteamVR_Controller.Input((int)controller.index); //get controller
-        if (canGrip && device.GetPress(SteamVR_Controller.ButtonMask.Grip)) // #TODO GetPRessDown?
-        {
-            Body.useGravity = false;
-            Body.isKinematic = true;
-            Body.transform.position += (prevPos - controller.transform.localPosition);
-
-        }else if(canGrip && device.GetPressUp(SteamVR_Controller.ButtonMask.Grip)) //some physics 
-        {
-            Body.useGravity = true;
-            Body.isKinematic = false;
-            Body.velocity = (prevPos - controller.transform.localPosition) / Time.deltaTime;
-        }
-        else
-        {
-            Body.useGravity = true;
-            Body.isKinematic = false;
-        }
-        //if (device.GetPress(SteamVR_Controller.ButtonMask.Grip))
-        //{
-        //    test = true;
-        //}
-        prevPos = controller.transform.localPosition;
     }
+	
 
-    void OnTriggerEnter()
+
+    void OnTriggerEnter(Collider other)
     {
-        canGrip = true;
+        if (other.gameObject.layer == LayerMask.NameToLayer("Grip")) //check if grippable object
+        {
+            Debug.Log("GRIPERINOO");
+            canGrip = true;
+        }
     }
     void OnTriggerExit()
     {
