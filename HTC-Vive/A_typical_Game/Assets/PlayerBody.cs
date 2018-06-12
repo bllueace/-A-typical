e5872663038efb8516dbc5f9b5003abbc49f58b8
Elvis <1600825@abertay.ac.uk>
2018-06-12 21:10:45 +0100

    Teleport blocking, improved climbing, player body

---
+++ HTC-Vive/A_typical_Game/Assets/PlayerBody.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBody : MonoBehaviour {
+
+    //public BoxCollider Player;
+    public BoxCollider Body;
+    public Transform Player;
+    
+    void Start ()
+    {
+        Player = Player.GetComponent<Transform>();
+        Body = Body.GetComponent<BoxCollider>();
+        
+    }
+    
+	void Update ()
+    {
+
+        //Body.transform.Translate(1,1,1);
+        //Body.center = new Vector3(10, 10, 10);
+        //Body.
+    }
+}
