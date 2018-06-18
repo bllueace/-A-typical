8c5c2b31b7e86c14547cddcd6dd9515867570224
bllueace <34141281+bllueace@users.noreply.github.com>
2018-06-18 07:40:15 +0100

    Combinable blocks, walk using trtack pad, heavy obj (not in use)

---
+++ HTC-Vive/A_typical_Game/Assets/Scripts/TooHeavy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooHeavy : MonoBehaviour
+{
+
+    public SteamVR_TrackedObject Controller;
+    public bool canPickUp;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "TooHeavy")
+        {
+            Debug.Log("TOO HEAVY?");
+            canPickUp = true;
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        canPickUp = false;
+    }
+
+}
