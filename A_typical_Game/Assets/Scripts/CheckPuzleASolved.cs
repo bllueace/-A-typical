579cc2c482f5076a572c661eeaed53f133098187
bllueace <34141281+bllueace@users.noreply.github.com>
2018-07-11 18:24:45 +0100

    PuzzleA(test)
    use mouse to rotate the block, once solved debug will print out text.
    
    To solve need to alight brown lines on the right hand corner

---
+++ A_typical_Game/Assets/Scripts/CheckPuzleASolved.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPuzleASolved : MonoBehaviour {
+
+
+    public RotatePuzleA piece1;
+    public RotatePuzleA piece2;
+    public RotatePuzleA piece3;
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+
+
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GameObject p1 = GameObject.Find("Cube_A_1");
+        piece1 = p1.GetComponent<RotatePuzleA>();
+
+        GameObject p2 = GameObject.Find("Cube_A_2");
+        piece2 = p2.GetComponent<RotatePuzleA>();
+
+        GameObject p3 = GameObject.Find("Cube_A_3");
+        piece3 = p3.GetComponent<RotatePuzleA>();
+
+
+        if (piece1.piece == true && piece2.piece == true && piece3.piece == true)
+        {
+            Debug.Log("Solved!");
+        }
+        //if(gameObject.name == "Cube_A_1" && (transform.rotation.y >= 1 && transform.rotation.y >= 350))
+        //{
+        //    Debug.Log("solved");
+        //}
+
+        //if (gameObject.name == "Cube_A_2" && (transform.rotation.y >= 1 && transform.rotation.y >= 350))
+        //{
+        //    Debug.Log("solved");
+        //}
+
+        //if (gameObject.name == "Cube_A_3" && (transform.rotation.y < 1 || transform.rotation.y > 359))
+        //{
+        //    Debug.Log("part 1 solved ");
+        //}
+
+
+    }
+}
---
+++ A_typical_Game/Assets/Scripts/RotatePuzleA.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePuzleA : MonoBehaviour
+{
+    /*other methods of rotation*/
+    //bool rotating = false;
+    //IEnumerator RotateMe(Vector3 byAngles, float inTime)
+    //{
+    //   // float difference;
+    //    var fromAngle = transform.rotation;
+    //    var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+    //    for (var t = 0f; t < 1f; t += Time.deltaTime / inTime)
+    //    {
+    //        transform.rotation = Quaternion.Lerp(fromAngle, toAngle, t);
+    //        yield return null;
+    //    }
+    //    rotating = false;
+    //}
+
+    //void OnMouseDown()
+    //{
+
+    //    if(!rotating)
+    //    {
+    //        rotating = true;
+    //        StartCoroutine(RotateMe(Vector3.up * 90, 1));
+    //    }
+    //}
+    ////////////////////////////////////////////////////////
+    //float speed = 90.0f;
+    //Vector3 vCurr = Vector3.zero;
+
+    //private Vector3 vNew = Vector3.zero;
+
+    //void Start()
+    //{
+    //    transform.eulerAngles = vCurr;
+    //    vNew = vCurr;
+    //}
+
+    //void Update()
+    //{
+    //    vCurr = Vector3.MoveTowards(vCurr, vNew, speed * Time.deltaTime);
+    //    transform.eulerAngles = vCurr;
+    //}
+
+    //void OnMouseDown()
+    //{
+    //    if (vCurr == vNew)
+    //        vNew.y += 90.0f;
+    //}  
+
+    float speed = 90.0f;
+    Quaternion quatern;
+
+    public bool piece = false;
+    //bool piece2 = false;
+    //bool piece3 = false;
+    //bool puzzleSolved = false;
+
+    void Start()
+    {
+        quatern = transform.rotation;        
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, quatern, speed * Time.deltaTime);
+
+        checkIfCorrect();
+    }
+
+    void checkIfCorrect()
+    {
+        if ((transform.rotation.y == 0))
+        {
+            piece = true;
+            //Debug.Log("part 1 " + piece1);
+        }
+        else
+        {
+            piece = false;
+        }
+        //if ((gameObject.name == "Cube_A_2") && (transform.rotation.y == 0))
+        //{
+        //    piece2 = true;
+        //    Debug.Log("part 2 " + piece2);
+        //}
+        //if ((gameObject.name == "Cube_A_1") && (transform.rotation.y == 0))
+        //{
+        //    piece3 = true;
+        //    Debug.Log("part 3 " + piece3);
+        //}
+        //if ((piece1 == true) && (piece2 == true) && (piece3 == true))
+        //{
+        //    puzzleSolved = true;
+        //    Debug.Log("All pieces correct!" + puzzleSolved);
+        //}
+    }
+
+    void OnMouseDown()
+    {
+        if (transform.rotation == quatern)
+        {
+            quatern = Quaternion.AngleAxis(90.0f, Vector3.up) * quatern;
+        }
+    }
+}
