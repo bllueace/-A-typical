54ea2d234723a0ee2ac29b5b056b7ffb88268ed5
bllueace <34141281+bllueace@users.noreply.github.com>
2018-07-26 19:03:08 +0100

    Bird Path update
    Use 'Spce' to trigger next location

---
+++ A_typical_Game/Assets/Scripts/Test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("Path1"), "time", 20, "easetype", iTween.EaseType.easeOutSine,"orientToPath", true, "lookTime", 0.5,"lookAhead",0.001,"oncomplete", "onPath1Complete"));
+    }
+	
+
+    void onPath1Complete()
+    {
+        //anim.SetBool("IDLE", true);
+
+        anim.Play(stateName: "FlyEnd");
+
+        Debug.Log("I have arived! *chirp* *chirp*");
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+    
+        if(Input.GetKeyDown("space"))
+        {
+            anim.Play(stateName: "FlyStart");
+
+            iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("Path2"), "time", 10, "easetype", iTween.EaseType.easeOutSine, "orientToPath", true,"lookTime", 0.5,"lookAhead",0.001, "oncomplete", "onPath1Complete"));
+
+        }
+
+    }
+}
