b1db670f6fbde5d694453e783c042a439108da33
bllueace <34141281+bllueace@users.noreply.github.com>
2018-08-07 20:30:56 +0100

    Bird Path update (test)

--- A_typical_Game/Assets/Scripts/Test.cs
+++ A_typical_Game/Assets/Scripts/Test.cs
@@ -5,36 +5,102 @@ using UnityEngine;
 public class Test : MonoBehaviour {
 
     Animator anim;
+    [SerializeField] Transform leftHand; //Is Camera(head)
+    [SerializeField] Transform rightHand; //Is Camera(head)
+    [SerializeField] Transform Bird; //Is Camera(head)
+    [SerializeField] Transform PickaxeInHand; // Script of the pickaxe in the hand
 
-    GameObject player;
+    public int currentPath = 0;
+    public GameObject leavingNarative;
+    bool flying = false;
+    float dist;
     // Use this for initialization
     void Start()
     {
-        player = GameObject.Find("Model");
         anim = GetComponent<Animator>();
 
-        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("Path1"), "time", 20, "easetype", iTween.EaseType.linear,"orientToPath", true, "lookTime", 0.5,"lookAhead",0.001,"oncomplete", "onPath1Complete"));
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("FromPitToNarrative"), "time", 3, "easetype", iTween.EaseType.linear,"orientToPath", true, "lookTime", 0.5,"lookAhead",0.001,"oncomplete", "onPathComplete"));
     }
 	
 
-    void onPath1Complete()
+    void onPathComplete()
     {
         //anim.SetBool("IDLE", true);
 
         anim.Play(stateName: "FlyEnd");
-
+        currentPath++;
+        flying = false;
         //Debug.Log("I have arived! *chirp* *chirp*");
     }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update() {
+
+        dist = Vector3.Distance(rightHand.position, Bird.transform.position);
+
+        Debug.Log("Distance to bird is: "+ dist);
+
+        if (currentPath == 1 && flying == false)
+        {
+            if ((Vector3.Distance(leftHand.position, Bird.transform.position) < 0.2f) || (Vector3.Distance(rightHand.position, Bird.transform.position) < 0.2f))
+            {
+                flying = true;
+                anim.Play(stateName: "Fly");
+                iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("FromNarrativeToPillar"), "time", 10, "easetype", iTween.EaseType.linear, "orientToPath", true, "lookTime", 0.5, "lookAhead", 0.001, "oncomplete", "onPathComplete"));
+            }
+            else if (Vector3.Distance(rightHand.position, Bird.transform.position) > 5f)
+            {
+                flying = true;
+                anim.Play(stateName: "Fly");
+                iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("FromNarrativeToPillar"), "time", 10, "easetype", iTween.EaseType.linear, "orientToPath", true, "lookTime", 0.5, "lookAhead", 0.001, "oncomplete", "onPathComplete"));
+            }
+        }
+
+        if (currentPath == 2 && !flying)
+        {
+            if ((Vector3.Distance(leftHand.position, Bird.transform.position) < 3f) || (Vector3.Distance(rightHand.position, Bird.transform.position) < 3f))
+            {
+                flying = true;
+                anim.Play(stateName: "Fly");
+                iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("FromPillarToPickaxe"), "time", 10, "easetype", iTween.EaseType.linear, "orientToPath", true, "lookTime", 0.5, "lookAhead", 0.001, "oncomplete", "onPathComplete"));
+            }
+        }
+
+        if (currentPath == 3 && !flying)
+        {
+            if ((Vector3.Distance(leftHand.position, PickaxeInHand.transform.position) < 0.2f) || (Vector3.Distance(rightHand.position, PickaxeInHand.transform.position) < 0.2f))
+            {
+                flying = true;
+                anim.Play(stateName: "Fly");
+                iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("FromPickaxeToCaventrance"), "time", 10, "easetype", iTween.EaseType.linear, "orientToPath", true, "lookTime", 0.5, "lookAhead", 0.001, "oncomplete", "onPathComplete"));
+            }
+
+        }
+
+        if (currentPath == 4 && !flying)
+        {
+            if ((Vector3.Distance(leftHand.position, PickaxeInHand.transform.position) < 3f) || (Vector3.Distance(rightHand.position, PickaxeInHand.transform.position) < 3f))
+            {
+                flying = true;
+                anim.Play(stateName: "Fly");
+                iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("FromCaveentranceToPuzzleA"), "time", 10, "easetype", iTween.EaseType.linear, "orientToPath", true, "lookTime", 0.5, "lookAhead", 0.001, "oncomplete", "onPathComplete"));
+            }
+
+        }
 
 
-        float dist = Vector3.Distance(player.transform.position, transform.position);
 
-        Vector3 dist2 = player.transform.position;
 
-        //Debug.Log("Distance to bird:" + dist2);
+
+
+
+
+
+
+
+
+
+
 
         if (Input.GetKeyDown("space"))
         {
